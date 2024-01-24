afbbcb31dab2775b77a8597a9e13d4203ca7cff3
augustosplett <augusto.splett@gmail.com>
2024-01-24 22:09:33 +0000

    implemented the function to create the cards results based on the return of the googlebooks api

--- script.js
+++ script.js
@@ -24,9 +24,27 @@ const getBookinformation = async (url) => {
     .then(response => response.json())
     .then(data => data.items.forEach(element => {
         console.log(element.volumeInfo.title)
+        console.log(element.volumeInfo.imageLinks.thumbnail)
+        createCard(element.volumeInfo.title, element.volumeInfo.imageLinks.thumbnail ? element.volumeInfo.imageLinks.thumbnail : element.volumeInfo.imageLinks.smallThumbnail)
     }))
 
 }
 const handleInputedString = (inputedString) => {
     return inputedString.replace(/\s+/g, '+');
+}
+
+const createCard = (title, url) => {
+    var result = document.querySelector(".container-result")
+    var card = document.createElement('div')
+    card.className = "card"
+    var element = `
+    <div class="book-cover">
+        <img src="${url}" alt="book-cover">
+    </div>
+    <div class="book-info">
+      ${title}
+      <input class= "blue-btn" type="button" value="see more">
+    </div>`
+  card.innerHTML = element
+  result.appendChild(card)
 }
